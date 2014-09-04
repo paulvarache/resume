f6ca713a78b80a9086a5e57c1e4b1f2d505ff1c7
Paul Varache <varache.paul@gmail.com>
2014-09-04 20:04:30 +0200

    Grunt prod correction

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,6 +7,8 @@ module.exports = function (grunt) {
             target: {
                 files: [
                     {src: "dev/build.html", dest: "prod/index.html"},
+                    {expand: true, cwd: "vendor/platform", src: ['**/**'], dest: "prod/vendor/platform"},
+                    {expand: true, cwd: "vendor/polymer", src: ['**/**'], dest: "prod/vendor/polymer/"},
                     {expand: true, cwd: "dev/resources/", src: ["**/**"], dest: "prod/resources/"}
                 ]
             }
